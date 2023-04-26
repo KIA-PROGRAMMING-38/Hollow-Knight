05163b6ff0fc88fe12e0a46a9a11882999121505
이찬혁 <120005279+lees5402@users.noreply.github.com>
2023-04-26 20:25:23 +0900

    [Feat] 플레이어 히트 이펙트 구현

--- Hollow Knight/Assets/Script/Player/DownSlashEffect.cs
+++ Hollow Knight/Assets/Script/Player/DownSlashEffect.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class DownSlashEffect : MonoBehaviour
 {
+    private HitEffect hitEffect;
+    private void Start()
+    {
+        hitEffect = GetComponentInChildren<HitEffect>();
+    }
     public void Show() => gameObject.SetActive(true);
     private void HideDownSlashEffect() => gameObject.SetActive(false);
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision != null && collision.CompareTag("Monster"))
+        {
+            hitEffect.Show();
+        }
+    }
 }
--- Hollow Knight/Assets/Script/Player/SlashEffect.cs
+++ Hollow Knight/Assets/Script/Player/SlashEffect.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class SlashEffect : MonoBehaviour
 {
+    private HitEffect hitEffect;
+    private void Start()
+    {
+        hitEffect = GetComponentInChildren<HitEffect>();
+    }
     // °ø°Ý ÀÌÆåÆ® »ý¼º
     public void Show() => gameObject.SetActive(true);
     // °ø°Ý ÀÌÆåÆ® ²ô±â
     private void HideSlashEffect() => gameObject.SetActive(false);
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision !=null && collision.CompareTag("Monster"))
+        {
+            hitEffect.Show();
+        }
+    }
 }

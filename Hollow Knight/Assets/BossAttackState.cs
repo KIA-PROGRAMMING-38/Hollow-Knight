66ce861f83230825b46dad5d8cef4df292730cb3
이찬혁 <120005279+lees5402@users.noreply.github.com>
2023-04-30 10:54:52 +0900

    [Feat] 몬스터 패턴 구현

--- Hollow Knight/Assets/BossAttackState.cs
+++ Hollow Knight/Assets/BossAttackState.cs
@@ -2,35 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossRunState : StateMachineBehaviour
+public class BossAttackState : StateMachineBehaviour
 {
     private BossController boss;
-    private Vector2 runVelocity = default;
-
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         boss = animator.GetComponent<BossController>();
-        runVelocity.Set(boss.moveSpeed, 0f);
-        if (boss.spriteRenderer.flipX == false)
-            boss.rigid.velocity = Vector2.left * runVelocity;
-        else
-            boss.rigid.velocity = Vector2.right * runVelocity;
     }
 
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if(boss.rigid.velocity.x == 0)
+        if(boss.bossHealth <= 500)
         {
-            animator.SetBool("Run", false);
-            animator.SetBool("Idle", true);
+            animator.SetTrigger("AttackRe");
         }
     }
 
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        animator.ResetTrigger("AttackRe");
     }
+
 
 }
